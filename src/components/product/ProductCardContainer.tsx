bfbe6b15a29941b04a0804c609714695ee2a104b
Afshin Ghezeli <a.g.afshin@gmail.com>
2024-11-03 23:38:00 +0330

    feat: implement productGrid

---
+++ src/components/product/ProductCardContainer.tsx
@@ -0,0 +1,11 @@
+import ProductCard from "./ProductCard";
+
+function ProductCardContainer() {
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      <ProductCard />
+    </div>
+  );
+}
+
+export default ProductCardContainer;
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,10 @@
+import ProductCardContainer from "../../components/product/ProductCardContainer";
+
 function HomePage() {
   return (
-    <div className="container">
-      <div className="h-96 bg-primary mt-12 rounded-2xl"></div>
+    <div className="container mt-12">
+      {/* <div className="h-96 bg-primary  rounded-2xl"></div> */}
+      <ProductCardContainer />
     </div>
   );
 }
