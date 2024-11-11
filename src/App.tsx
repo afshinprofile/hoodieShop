d8af487f79634af64a6202425cce86d4d2daf91c
Afshin Ghezeli <a.g.afshin@gmail.com>
2024-11-11 11:00:00 +0330

    setting up react-query provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import ReactQueryProvider from "./providers/reactQueryProvider";
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <ReactQueryProvider>
+        <RouterProvider router={router} />
+      </ReactQueryProvider>
     </>
   );
 }
