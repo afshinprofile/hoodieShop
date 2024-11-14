bcfe3eae1ff235e6e1c6ccb2f11be0b7d63699e8
Afshin Ghezeli <a.g.afshin@gmail.com>
2024-11-14 11:00:00 +0330

    fetch product

--- src/pages/product/ProductPage.tsx
+++ src/pages/product/ProductPage.tsx
@@ -1,34 +1,38 @@
 import { ShoppingCart } from "iconsax-react";
+import { Link, useParams } from "react-router-dom";
 import Button from "../../components/button/Button";
-import hoodie1 from "/products/hoodie1.webp";
-import { Link } from "react-router-dom";
+import ImageLoader from "../../components/ImageLoader";
+import useProducts from "../../hooks/useProducts";
 function ProductPage() {
+  const { id } = useParams();
+  const { data } = useProducts();
+  const product = data?.find((item) => item.id === Number(id));
+
   return (
     <div>
       <div className="container mt-12">
         <div className="grid grid-cols-12 gap-10">
-          <div className="col-span-4 border border-slate-300 rounded-2xl p-3">
-            <img src={hoodie1} alt="hoodie1" className="object-cover" />
+          <div className="col-span-4 border border-slate-300 rounded-2xl p-3 h-96">
+            <div className="flex items-center justify-center h-full">
+              {product?.img_url && product.img_placeholder && (
+                <ImageLoader
+                  src={product?.img_url}
+                  hash={product?.img_placeholder}
+                  alt=""
+                />
+              )}
+            </div>
           </div>
           {/* product details */}
           <div className="col-span-8">
             <div className="flex flex-col gap-5 h-full">
               {/* product name */}
-              <h1 className="font-EstedadBold text-3xl">
-                هودی مردانه مانگو مدل FG902BON
-              </h1>
+              <h1 className="font-EstedadBold text-3xl">{product?.name}</h1>
               {/* product description */}
-              <p className="font-EstedadRegular ">
-                هودی جلو بسته و کلاهدار مردانه سبز (FOREST GREEN) تهیه شده از
-                67% الیاف نخی و 33% پلی استر دارای دو جیب در طرفین کلاه دارای
-                بند قابل تنظیم دارای پارچه لطیف، داخل کرکی و گرم لبه آستین ها و
-                پایین لباس کشبافت دارد دارای فرم معمولی (REGULAR FIT) مناسب برای
-                استفاده روزمره مدل در عکسها سایز M را پوشیده است جهت انتخاب سایز
-                مناسب به جدول راهنمای سایز در عکسها مراجعه نمایید
-              </p>
+              <p className="font-EstedadRegular ">{product?.description}</p>
               {/* product add to cart button */}
               <div className="w-72 mt-auto">
-                <Link to={'/cart'}>
+                <Link to={"/cart"}>
                   <Button variant="primary" icon={<ShoppingCart />}>
                     افزودن به سبد خرید
                   </Button>
